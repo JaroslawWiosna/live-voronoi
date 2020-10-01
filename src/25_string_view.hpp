ee5a1c4a20e41bcdf0b6d1410dd652678eaea112
Jarosław Wiosna <jaroslaw.wiosna.github@gmail.com>
2020-10-01 19:47:25 +0200

    StringView comment

--- src/25_string_view.hpp
+++ src/25_string_view.hpp
@@ -1,5 +1,9 @@
 #pragma once
 
+// This is a lite version of std::string_view
+// https://code.woboq.org/gcc/libstdc++-v3/include/std/string_view.html#70
+//
+// even len and str names are the same as std's
 struct StringView {
     size_t len;
     const char *str;
